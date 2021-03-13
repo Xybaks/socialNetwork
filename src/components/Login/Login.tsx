7811c39d8f38be5c4f4f0f03a377668fc0e09acd
Xybaks <nikolai.berestevich@gmail.com>
2021-03-13 13:24:22 +0300

    Form was added to Login-page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,26 +1,40 @@
 import React from "react";
+import {reduxForm, Field, InjectedFormProps} from "redux-form";
 
-const Login = () => {
-    return <div>
-        <h1>LOGIN</h1>
-        <div>Just do it! ^^</div>
-        <LoginForm/>
-    </div>
+type FormDataType = {
+    login: string,
+    password: string,
+    rememberMe: boolean
 }
 
-const LoginForm = () => {
-    return <form>
+
+const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+    return <form onSubmit={props.handleSubmit}>
         <div>
-            <input placeholder="login"/>
+            <Field placeholder="Login" name={"login"} component={"input"}/>
         </div>
         <div>
-            <input placeholder="password"/>
+            <Field placeholder="Password" name={"password"} component={"input"}/>
         </div>
         <div>
-            <input type="checkbox"/>
+            <Field type="checkbox" name={"rememberMe"} component={"input"}/>
         </div>
         <button>LogIn</button>
     </form>
 }
 
-export default Login
+
+const LoginReduxForm = reduxForm<FormDataType>({form: "login"})(LoginForm)
+
+const Login = () => {
+    const onSubmit = (formData: FormDataType) => {
+        console.log(formData)
+    }
+    return <div>
+        <h1>LOGIN</h1>
+        <div>Just do it! ^^</div>
+        <LoginReduxForm onSubmit={onSubmit}/>
+    </div>
+}
+
+export default Login;
